1bb0144e6fbd1d9778cfd2e8ef2daa882ad79fb2
ajitesh <ajitesh314mishra@gmail.com>
2022-10-07 14:16:51 +0530

    4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,68 +1,77 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-
-  const [state, setState] = useState(false)
-
-  // Replace javascript:void(0) path with your path
-  const navigation = [
-      { title: "Customers", path: "javascript:void(0)" },
-      { title: "Careers", path: "javascript:void(0)" },
-      { title: "Guides", path: "javascript:void(0)" },
-      { title: "Partners", path: "javascript:void(0)" }
-  ]
-
   return (
-      <nav className="bg-white w-full border-b md:border-0 md:static">
-          <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
-              <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                    <a href="javascript:void(0)">
-                        <img
-                            src="https://www.floatui.com/logo.svg" 
-                            width={120} 
-                            height={50}
-                            alt="Float UI logo"
-                        />
-                    </a>
-                  <div className="md:hidden">
-                      <button className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
-                          onClick={() => setState(!state)}
-                      >
-                          {
-                              state ? (
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                                      <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                                  </svg>
-                              ) : (
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8h16M4 16h16" />
-                                  </svg>
-                              )
-                          }
-                      </button>
-                  </div>
-              </div>
-              <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${ state ? 'block' : 'hidden'}`}>
-                  <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                      {
-                          navigation.map((item, idx) => {
-                              return (
-                                <li key={idx} className="text-gray-600 hover:text-indigo-600">
-                                    <a href={item.path}>
-                                        { item.title }
-                                    </a>
-                                </li>
-                              )
-                          })
-                      }
-                  </ul>
-              </div>
-              <div className="hidden md:inline-block">
-                <a href="javascript:void(0)" className="py-3 px-4 text-white bg-indigo-600 hover:bg-indigo-700 rounded-md shadow">
-                    Get Started
-                </a>
-              </div>
-          </div>
-      </nav>
-  )
-}
+    <div class="navbar bg-base-100 mx-auto justify-center">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li tabindex="0">
+              <a class="justify-between">
+                Parent
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul class="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <Link to={'/'} class="btn btn-ghost normal-case text-xl">AGGRANT</Link>
+      </div>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="flex justify-between horizontal p-0">
+          <li>
+            <Link className="mx-2 px-2 hover:text-indigo-700 font-semibold font-mono" to={"/"}>Home</Link>
+          </li>
+
+          <li>
+            <Link className="mx-2 px-2 hover:text-indigo-700 font-semibold font-mono" to={"/course"}>Courses</Link>
+          </li>
+          <li>
+            <a className="mx-2 px-2 hover:text-indigo-700 font-semibold font-mono" href={"https://github.com/Iamam305/aggrant"}>Github</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -3,47 +3,56 @@ import { Courses } from "../helpers/db";
 
 const Course = () => {
   return (
-    <div className=" md:w-3/4 mx-auto">
-      {Courses?.map((course) => (
-        <div class="collapse my-4  collapse-plus border border-base-300 bg-white shadow-md rounded-box">
-          <input type="checkbox" class="peer" />
-          <div class="collapse-title font-bold text-2xl ">
-          {course.Name}
-          </div>
-          <div class="collapse-content ">
-            {course.content?.map((video) => (
-              <>
-                <label htmlFor="my-modal" className="block modal-button p-2 m-4 cursor-pointer shadow rounded-md">
-                  {video.name}
-                </label>
+    <div className=" p-4 md:w-3/4 mx-auto">
+      {Courses?.map((course, index) => (
+        <>
+          <div class=" collapse my-4 px-4 collapse-arrow border border-base-300 bg-white shadow-md rounded-box">
+            <input type="checkbox" class="peer" />
+            <div class="collapse-title    font-medium text-lg ">
+              <span className="badge badge-secondary mr-2">
+              {index + 1}
+                </span> {course.Name}
+            </div>
+            <ul class="collapse-content steps steps-vertical">
+              {course.content?.map((video, index) => (
+                <>
+                  <li className="step  step-primary">
+                    <label
+                      htmlFor={`my-modal${video.url}`}
+                      className="block modal-button py-2 border px-8 m-4 cursor-pointer shadow rounded-md"
+                    >
+                      {video.name}
+                    </label>
+                  </li>
 
-                <input type="checkbox" id="my-modal" className="modal-toggle" />
-                <div className="modal  ">
-                  <div className="modal-box p-2 md:p-6  max-w-[960px]">
-                     <div className= "modal-action text-red-500 inline absolute right-6 top-1 mt-0">
-                      <label htmlFor="my-modal" >
-                        X
-                      </label>
+                  <input
+                    type="checkbox"
+                    id={`my-modal${video.url}`}
+                    className="modal-toggle"
+                  />
+                  <div className="modal  ">
+                    <div className="modal-box p-2 md:p-6  max-w-[960px]">
+                      <div className="modal-action text-red-500 inline absolute right-6 top-1 mt-0">
+                        <label htmlFor={`my-modal${video.url}`}>X</label>
+                      </div>
+                      <div className="">
+                        <iframe
+                          className="aspect-video w-full mx-auto"
+                          src={video.url}
+                          title="YouTube video player"
+                          frameborder="0"
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                          allowfullscreen
+                        ></iframe>
+                      </div>
                     </div>
-                    <div className="">
-
-                    
-                    <iframe
-                     className="aspect-video w-full mx-auto"
-                      src={video.url}
-                      title="YouTube video player"
-                      frameborder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowfullscreen
-                    ></iframe>
-                    </div>
-                   
                   </div>
-                </div>
-              </>
-            ))}
+                </>
+              ))}
+            </ul>
           </div>
-        </div>
+          <div class="divider"></div>
+        </>
       ))}
     </div>
   );
